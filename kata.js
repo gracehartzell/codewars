9a6fdc86b9ece6dc39689125d5b17708c0b98361
Grace Hartzell <gracehartzell@utexas.edu>
2018-07-06 22:15:24 -0500

    Added notes to kata

--- kata.js
+++ kata.js
@@ -31,3 +31,23 @@ i makes case insensitive
 Used EJS, MDN, and above URL for reference with regex
 */
 
+/* ADDING TO GITHUB (to this repo)
+    ...cd codewars (name of directory)
+    git commit -m "First 8kyu on this"
+        >> [master] First 8kyu on this
+        >> 1 file changed, 33 insertions(+)
+    git push 
+        >>  Counting objects: 3, done.
+            Delta compression using up to 4 threads.
+            Compressing objects: 100% (2/2), done.
+            Writing objects: 100% (3/3), 751 bytes | 751.00 KiB/s, done.
+            Total 3 (delta 0), reused 0 (delta 0)
+            To https://github.com/gracehartzell/codewars.git
+                d1d215c..18df35f  master -> master
+
+    According to http://dont-be-afraid-to-commit.readthedocs.io/en/latest/git/commandlinegit.html
+    you only need to type git push since the repository exists at both ends (but to still be explicit 
+    in what you want; therefore, on the next push I'll write it in full)
+*/
+// QUESTION: master is this particular branch name?
+
