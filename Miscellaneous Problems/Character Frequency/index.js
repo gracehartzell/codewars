b8c325b875f14557473b372c2a77a1ed6f7a0695
Grace Hartzell <gracehartzell@utexas.edu>
2019-01-14 08:57:33 -0600

    Added new problem

--- Miscellaneous Problems/Character Frequency/index.js
+++ Miscellaneous Problems/Character Frequency/index.js
@@ -1,3 +1,50 @@
+const characterFrequency = str => {
+  let countObject = str.split("").reduce((acc, val) => {
+    acc[val] ? acc[val]++ : (acc[val] = 1);
+    return acc;
+  }, {});
+
+  // const keys = Object.keys(count);
+  const arrays = Object.keys(countObject).map(key => [key, countObject[key]]);
+
+  return arrays.sort((a, b) => {
+    return b[1] - a[1] || a[0].charCodeAt(0) - b[0].charCodeAt(0);
+  });
+};
+
+let str1 = "mississippi"  // [ [ "i", 4 ], [ "s", 4 ], [ "p", 2 ], [ "m", 1 ] ]
+let str2 = "aaabbc" // [ [ "a", 3 ], [ "b", 2 ], [ "c", 1 ] ]
+let str3 = "" // []
+
+console.log(characterFrequency(str1))
+console.log(characterFrequency(str2))
+console.log(characterFrequency(str3))
+
+
+const characterFreq = string => {
+  const count = {};
+
+  string.split("").forEach(char => {
+    count[char] = (count[char] || 0) + 1;
+  });
+
+  const keys = Object.keys(count);
+  const arrays = keys.map(key => {
+    return [key, count[key]];
+  });
+
+  return arrays.sort((curr, next) => {
+    return next[1] - curr[1] || curr[0].charCodeAt(0) - next[0].charCodeAt(0);
+  });
+};
+
+console.log(characterFreq(str1))
+console.log(characterFreq(str2))
+console.log(characterFreq(str3))
+
+
+
+/* MISC. ATTEMPTS/NOTES?
 const characterFreq = num => {
   const arr = string.split("");
   const count = {};
@@ -51,3 +98,8 @@ function sortF(a, b) {
     return b[1] - a[1];
   }
 }
+ 
+
+
+
+ */
