a38efc77c9624df472da86279b15fa59a597c47b
Grace Hartzell <gracehartzell@utexas.edu>
2019-01-14 12:57:07 -0600

    Setup for tree practice

---
+++ Miscellaneous Problems/Tree Practice/README.md
@@ -0,0 +1,15 @@
+# Binary Search Tree Creation
+
+In this repo, you will find a directory for test (which houses the test for the tree) and a template file for the creation of the tree. 
+
+## How to Use:
+#### If this was a standalone directory:
+* Fork and clone
+* Complete the following commands in your †erminal:
+```shell
+git clone https://github.com/gracehartzell/DIRECTORY-NAME
+cd DIRECTORY-NAME
+npm install 
+
+```
+<sup>*</sup>Instead of the dummy link, you'd copy+paste the actual SSH for the clone of the repository. 
---
+++ Miscellaneous Problems/Tree Practice/Tree.js
@@ -0,0 +1,64 @@
+// ==============================
+// PLEASE READ THE USAGE BELOW BEFORE WRITING CODE
+// The goal is for you to write the code for the Tree structure 
+// so that it can be used by others in the examples below:
+// ==============================
+
+// Creating the tree && connecting nodes
+// ==================
+// let myTree = new Tree(5);
+// myTree.addNode(1);
+// myTree.addNode(10);
+
+// VISUAL:     5
+//            / \
+//           1   10
+
+// Adding grand children:
+// ==================
+// myTree.addNode(-5) 
+// myTree.addNode(2)
+// myTree.addNode(8)
+// myTree.addNode(50)
+//
+// VISUAL:        5
+//             /    \
+//            1      10
+//          /  \    /  \
+//        -5    2   8   50
+
+//printBreadthFirst:
+//==================
+// VISUAL:        5
+//             /    \
+//            1      10
+//          /  \    /  \
+//        -5    2   8   50
+//myTree.printBreadthFirst()
+//5,1,10,-5,2,8,50
+
+// Removing children:
+// ==================
+// VISUAL:        5
+//             /    \
+//            1      10
+//          /  \    /  \
+//        -5    2  8    50
+
+// myTree.removeNode(10)
+// VISUAL:       5
+//             /   \
+//            1     8
+//          /  \     \
+//        -5    2     50
+
+class Tree {
+  // implement the following :
+  // Properties:
+  // value -> returns value of node it was given when it was created
+  // children -> returns direct children of a node as an array
+  // Methods: 
+  // addChild()
+  // printBreadthFirst()
+  // removeChild()
+};
