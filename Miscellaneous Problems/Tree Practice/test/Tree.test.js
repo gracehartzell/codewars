94b2f71e217bc66e0e33fa85e8a100e5c88c4dd9
Grace Hartzell <gracehartzell@utexas.edu>
2019-01-14 14:03:09 -0600

    Tests done

--- Miscellaneous Problems/Tree Practice/test/Tree.test.js
+++ Miscellaneous Problems/Tree Practice/test/Tree.test.js
@@ -3,17 +3,6 @@ const Tree = require("../Tree");
 
 let myTree = new Tree(5);
 
-/*
-  implement the following :
-  Properties:
-  value -> returns value of node it was given when it was created
-  children -> returns direct children of a node as an array
-  Methods: 
-  addChild()
-  printBreadthFirst()
-  removeChild()
-*/
-
 describe("Tree testing", () => {
   describe("Creation of tree", () => {
     it("should create a new instance of Tree", () => {
@@ -39,6 +28,40 @@ describe("Tree testing", () => {
       expect(myTree.children[0].children[1]).to.eql(2);
       expect(myTree.children[1].children[0]).to.eql(8);
       expect(myTree.children[1].children[1]).to.eql(50);
+    });
+  });
+
+  describe("Breadth First Search", () => {
+    it("should print all nodes in BFS order", () => {
+      myTree.addNode(1);
+      myTree.addNode(10);
+      myTree.addNode(-5);
+      myTree.addNode(2);
+      myTree.addNode(8);
+      myTree.addNode(50);
+
+      expect(myTree.printBreadthFirst()).to.eql(5, 1, 10, -5, 2, 8, 50);
+    });
+  });
+
+  describe("Removal of child", () => {
+    it("should remove a child with given value", () => {
+      myTree.addNode(1);
+      myTree.addNode(10);
+      myTree.addNode(-5);
+      myTree.addNode(2);
+      myTree.addNode(8);
+      myTree.addNode(50);
+
+      myTree.removeChild(8);
+
+      expect(myTree.children[1]).to.eql([
+        { value: 10, children: [{ value: 50, children: null }] }
+      ]);
+
+      myTree.removeChild(50);
+
+      expect(myTree.children[1]).to.eql([{ value: 10, children: null }]);
     });
   });
 });
