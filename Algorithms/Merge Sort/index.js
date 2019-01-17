807de9cfb59c7fc52d7799887630e240a0b06288
Grace Hartzell <gracehartzell@utexas.edu>
2019-01-17 16:06:12 -0600

    MergeSort tests passing :tada:

---
+++ Algorithms/Merge Sort/index.js
@@ -0,0 +1,30 @@
+// Step 1: Split array into halves then recursively merge
+const mergeSort = arr => {
+  if (arr.length === 1) return arr;
+
+  const middle = Math.floor(arr.length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+
+  return merge(mergeSort(left), mergeSort(right));
+};
+
+// Step 2: Compare and concatenate!
+const merge = (left, right) => {
+  const result = [];
+  let indexLeft = 0;
+  let indexRight = 0;
+
+  while (indexLeft < left.length && indexRight < right.length) {
+    if (left[indexLeft] < right[indexRight]) {
+      result.push(left[indexLeft]);
+      indexLeft++;
+    } else {
+      result.push(right[indexRight]);
+      indexRight++;
+    }
+  }
+  return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
+};
+
+module.exports = mergeSort;
