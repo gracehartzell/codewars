0d4dcd6b997550b21a6ec47e38f3312474c07357
Grace Hartzell <gracehartzell@utexas.edu>
2019-02-17 14:55:10 -0600

    Game added

--- Recursion/Call Stack Walkthrough/CallStackGame.js
+++ Recursion/Call Stack Walkthrough/CallStackGame.js
@@ -1,11 +1,10 @@
-const callMe = () => {
-  callMe();
-  callMe();
-  callMe("anytime");
+const callMeInfinite = () => {
+  callMeInfinite();
+  callMeInfinite();
+  callMeInfinite("anytime");
 };
 
-callMe(); // function gets pushed to call stack
-
+// callMeInfinite(); // function gets pushed to call stack
 
 /*
 CALL STACK:
@@ -14,4 +13,57 @@ CALL STACK:
   * Line 2 = another function call => Push that function to CS 
   * Execute function body...
   * Line 2 --> Stuck in infinite loop because there's never a return
+*/
+
+let tracker = 0;
+const callMe = (arg) => {
+  tracker++;
+  if (tracker === 3) {
+    tracker = 0;
+    return `loops! ARG: ${arg}`;
+  }
+  return callMe("anytime");
+};
+
+console.log(callMe());
+
+/*
+FIRST CALL: 
+* function put on call stack:
+
+  let tracker = 0; 
+  const callMe = (arg) => {         // arg: "undefined" (hasn't hit the "anytime" arg)
+    tracker++; // tracker now = 1
+    if (tracker === 3) {            // condition is currently false
+      tracker = 0;
+      return `loops! ARG: ${arg}`; 
+    }
+    return callMe("anytime");
+  };
+
+SECOND TIME:
+
+  let tracker = 0;
+  const callMe = (arg) => {         // arg now = "anytime"
+    tracker++;                      // tracker now = 2
+    if (tracker === 3) {            // still false ==> called again
+      tracker = 0;
+      return `loops! ARG: ${arg}`;
+    }
+    return callMe("anytime");
+  };
+
+THIRD TIME:
+
+  let tracker = 0;                  // tracker = 2 
+  const callMe = (arg) => {         // arg still = "anytime"
+    tracker++;                      // tracker finally = 3
+    if (tracker === 3) {
+      tracker = 0;
+      return `loops! ARG: ${arg}`;  // will now get returned
+    }
+    return callMe("anytime");
+  };
+
+
 */
