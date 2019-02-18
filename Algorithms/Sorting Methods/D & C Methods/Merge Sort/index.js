963288525a95bc564076664f1e4dfa127ef3a20c
Grace Hartzell <gracehartzell@utexas.edu>
2019-02-18 13:11:13 -0600

    Practiced bubble and merge

--- Algorithms/Sorting Methods/D & C Methods/Merge Sort/index.js
+++ Algorithms/Sorting Methods/D & C Methods/Merge Sort/index.js
@@ -1,7 +1,6 @@
 // Step 1: Split array into halves then recursively merge
 const mergeSort = arr => {
   if (arr.length === 1) return arr;
-
   const middle = Math.floor(arr.length / 2);
   const left = arr.slice(0, middle);
   const right = arr.slice(middle);
@@ -28,3 +27,48 @@ const merge = (left, right) => {
 };
 
 module.exports = mergeSort;
+
+
+
+ 
+const mergeSP = arr => {
+  // base case
+  if (arr.length === 1) return arr;
+
+  //get middle
+  const middle = Math.floor(arr.length / 2);
+  // get left array
+  const left = arr.slice(0, middle);
+  // get right array
+  const right = arr.slice(middle);
+  // return: run merge on sorted left and sorted right arrays
+  return merge(mergeSP(left), mergeSP(right));
+}
+
+const mergeP = (left, right) => {
+  // initialize empty results array
+  let result = [];
+  // initialize starting points for left and right arrays
+  let indexLeft = 0;
+  let indexRight = 0;
+  // while the count of left/right indices are less than their lengths {
+  while (indexLeft < left.length && indexRight < right.length) {
+  // if 0th number of left is less than 0th of right, 
+    if (left[indexLeft] < right[indexRight]) {
+      // push the left number into the results array
+      result.push(left[indexLeft]);
+      // increment the left index
+      indexLeft++;
+    } else {
+      // push the right number into the results array
+      result.push(righ[indexRight]);
+      // increment the right index
+      indexRight++;
+    }
+  }
+    // return the concatenated results array with the left and right slices.
+    return result.concat(left.slice(indexLeft)).concat(right.slice(indexRight));
+}
+
+var arrayRandom = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+console.log(mergeSP(arrayRandom));
--- Algorithms/Sorting Methods/Naive Methods/Bubble Sort/index.js
+++ Algorithms/Sorting Methods/Naive Methods/Bubble Sort/index.js
@@ -1,16 +1,16 @@
-const bubbleSort1 = nums => {  
+const bubbleSort1 = nums => {
   let swapped;
   do {
     swapped = false;
     for (var i = 0; i < nums.length; i++) {
-      if (nums[i] > nums[i+1]) {
+      if (nums[i] > nums[i + 1]) {
         var temp = nums[i];
-        nums[i] = nums[i+1];
-        nums[i+1] = temp;
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
         swapped = true;
       }
     }
-  } while(swapped);
+  } while (swapped);
   return nums;
 };
 
@@ -21,28 +21,47 @@ const bubbleSort2 = arr => {
   while (!done) {
     done = true;
     for (let i = 1; i < arr.length; i++) {
-      if (arr[i-1] > arr[i]) {
+      if (arr[i - 1] > arr[i]) {
         done = false;
-        [arr[i-1], arr[i]] = [arr[i], arr[i-1]]
+        [arr[i - 1], arr[i]] = [arr[i], arr[i - 1]];
       }
     }
   }
   return arr;
-}
+};
 
 const bubbleSort3 = arr => {
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > arr[i+1]) {
+    if (arr[i] > arr[i + 1]) {
       let temp = arr[i];
-      arr[i] = arr[i+1];
-      arr[i+1] = temp;
-      return bubbleSort1(arr)
+      arr[i] = arr[i + 1];
+      arr[i + 1] = temp;
+      return bubbleSort1(arr);
     }
   }
-}
+};
 
+var arrayRandom = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+console.log(bubbleSort3(arrayRandom));
 
 console.log(bubbleSort1([5, 7, 6, 4]));
 console.log(bubbleSort2([5, 7, 6, 4]));
 console.log(bubbleSort3([5, 7, 6, 4]));
 
+const bsP = arr => {
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] > arr[i + 1]) {
+        let temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return arr;
+};
+console.log(bsP(arrayRandom));
