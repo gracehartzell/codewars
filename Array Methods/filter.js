2931c6bd5a86a7b73a4ff0974ea320dd6c107c92
Grace Hartzell <gracehartzell@utexas.edu>
2019-02-26 21:02:05 -0600

    Added while and string

--- Array Methods/filter.js
+++ Array Methods/filter.js
@@ -1,5 +1,8 @@
 const input = [8, 6, 7, 5, 3, 0, 9];
+const strs = ['Eren', 'Mikasa', 'Armin', 'Annie'];
+console.log(input.filter(el => el % 3 === 0)); // [6, 3, 0, 9]
 
+// FILTER WITH FOR LOOP, O(n)
 const filterFor = (arr, cb) => {
   let filtered = [];
   for (let i = 0; i < arr.length; i++) {
@@ -8,6 +11,18 @@ const filterFor = (arr, cb) => {
   return filtered;
 };
 
+console.log(filterFor(input, el => el % 3 === 0)); // [6, 3, 0, 9]
 
-console.log(input.filter(el => el % 3 === 0)); // [6, 3, 0, 9]
-console.log(filterFor(input, (el) => el % 3 === 0)) // [6, 3, 0, 9]
+// FILTER WITH WHILE LOOP
+const filterWhile = (arr, cb) => {
+  let filtered = [];
+
+  let i = 1;
+  while (i < arr.length) {
+    if (cb(arr[i])) filtered.push(arr[i]);
+    i++;
+  }
+  return filtered;
+}
+
+console.log(filterWhile(input, el => el % 3 === 0)); // [6, 3, 0, 9]
